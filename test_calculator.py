c1636bf7d437e8d9386e1daee600efbe0a8d5424
Chunwei05 <chunwei100605@gmail.com>
2025-09-17 15:30:36 +0800

    First Test - Initial State

--- test_calculator.py
+++ test_calculator.py
@@ -1,1 +1,9 @@
+import pytest
+from src.calculator import Calculator
 
+class TestCalculatorInitialState:
+    
+    def test_initial_answer_is_zero(self):
+        """Test that a new calculator starts with answer 0"""
+        calc = Calculator()
+        assert calc.get_answer() == 0
